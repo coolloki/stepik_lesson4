f0a737da0b668a4a4f7d153885aba3fcd2284594
coolloki <cooool.loki@gmail.com>
2020-06-26 17:07:32 +0600

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Применение паттерна Page Object Model
---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru',
+                     help='Choose the language, like en - English')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    chosen_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': chosen_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
+
